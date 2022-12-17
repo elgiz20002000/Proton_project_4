ef4110267307db7ea934d8f0a7717214910d0297
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2022-12-17 11:10:08 +0400

    commit

--- src/js/top_nav.js
+++ src/js/top_nav.js
@@ -1,7 +1,7 @@
 export const top_nav  = () => {
     const navbar = document.querySelector('.top_navbar')
     window.addEventListener('scroll' , () => {
-            if(window.scrollY >= document.body.scrollHeight * 0.1 && !document.querySelector(".menu_overlay").classList.contains('active') & 
+            if(window.scrollY >= document.body.scrollHeight * 0.1 &&  
             !document.querySelector(".search_overlay").classList.contains('active')) {
                 navbar.classList.add('active')
 
